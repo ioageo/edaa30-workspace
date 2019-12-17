e54024c3997a449503176e6347d78ce374ce307d
ioageo <58464269+ioageo@users.noreply.github.com>
2019-12-17 09:23:19 +0100

    fixed uppgift

--- Uppgift/src/MainFX.java
+++ Uppgift/src/MainFX.java
@@ -12,12 +12,13 @@ import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class MainFX extends Application {
-
+	
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		String color = "orange";
+		//the color of the grid
+		final String color = "orange";
 		int[][] matrix = new int[9][9];
-
+		soduku_class sol = new soduku_class();
 		/*
 		 * Initiate screen
 		 */
@@ -26,12 +27,11 @@ public class MainFX extends Application {
 		grid.setVgap(1);
 		grid.setHgap(1);
 		
-		/*
-		 * initial screen 
-		 */
+	/*
+	 * Creates a TextField matrix
+	 */
 		for (int y = 0; y < 9; y++) {
 			for (int x = 0; x < 9; x++) {
-
 				TextField t = new TextField();
 				t.setEditable(true);
 				t.setPrefSize(100, 100);
@@ -41,20 +41,23 @@ public class MainFX extends Application {
 				}
 				final int a = y;
 				final int b = x;
+				/*
+				 * Checks and applies the rules for input
+				 */
 				t.setOnKeyReleased(Event -> {
 					String s = t.getText();
 					int nbr;
 					try {
 						nbr = Integer.parseInt(s);
-						ExtraMethod em = new ExtraMethod();
-						if (nbr < 1 || nbr > 9 || !em.colrow(matrix, nbr, b, a) ) {
+						if (nbr < 1 || nbr > 9 || !sol.checkRules(matrix,a,b,nbr ) ) {
 							Alert al = new Alert(AlertType.INFORMATION);
 							al.setContentText("You wrote wrong number ");
 							al.setHeaderText("ERROR");
 							al.show();
-							t.clear();
+							t.clear();							
 							nbr = 0;
 						}
+						
 					} catch (NumberFormatException nfe) {
 						Alert al = new Alert(AlertType.INFORMATION);
 						al.setContentText("I want only digits ");
@@ -62,11 +65,15 @@ public class MainFX extends Application {
 						al.show();
 						t.clear();
 						nbr = 0;
+						
 					}
 					if(nbr!=0)
 					matrix[a][b] = nbr;
 
 				});
+				/*
+				 * Alternates the color between boxes
+				 */
 				if (y / 3 == 0 && x / 3 != 1 || y / 3 == 2 && x / 3 != 1 || y / 3 == 1 && x / 3 == 1)
 					t.setStyle("-fx-control-inner-background: " + color + ";");				
 				GridPane.setConstraints(t, y, x);
@@ -78,16 +85,20 @@ public class MainFX extends Application {
 		
 
 		BorderPane root = new BorderPane();
-
+		/*
+		 * Initiate buttons 
+		 */
 		Button solve = new Button("Solution");
 		solve.setMaxWidth(Double.MAX_VALUE);
 		solve.setMinWidth(75);
 		Button clear = new Button("Clear");
 		clear.setMaxWidth(Double.MAX_VALUE);
 		clear.setMinWidth(75);
-		
+		/*
+		 * Solves sudoku and prints it
+		 */
 		solve.setOnAction(Event -> {
-			soduku_class sol = new soduku_class(matrix);
+			sol.SetMatrix(matrix);
 			int[][] mat = sol.SolvedMatrix();		
 			if(mat!=null) {
 			grid.getChildren().clear();
@@ -114,7 +125,9 @@ public class MainFX extends Application {
 			}
 		});
 		
-		
+		/*
+		 * Cleares the TextField matrix
+		 */
 		clear.setOnAction(Event->{			
 					grid.getChildren().clear();
 					grid.setPadding(new Insets(5, 5, 5, 5));
--- Uppgift/src/soduku_class.java
+++ Uppgift/src/soduku_class.java
@@ -1,132 +1,32 @@
 
 public class soduku_class {
-	public int[][] sudokuMatrix = new int[9][9];
+	private int[][] sudokuMatrix;
 
-	soduku_class(int[][] a) {
-		for (int i = 0; i < a.length; i++) {
-			for (int k = 0; k < a[0].length; k++) {
-				sudokuMatrix[i][k] = a[i][k];
+	/**
+	 * Class constructor
+	 */
+	public soduku_class() {
+		sudokuMatrix = new int[9][9];
+	}
+
+	/**
+	 * Sets the numbers in sudokuMatrix
+	 * 
+	 * @param a given matrix
+	 */
+	void SetMatrix(int[][] matrix) {
+		for (int i = 0; i < matrix.length; i++) {
+			for (int k = 0; k < matrix[0].length; k++) {
+				sudokuMatrix[i][k] = matrix[i][k];
 			}
 		}
 	}
 
 	/**
-	 * Solves the puzzle, if it could be solved .
-	 * 
-	 * @return 0 if the puzzle can't be solved, 1 if the puzzle can be solved.
+	 * Solves the sudoku if possible
 	 */
 	public void solve() {
 		solve(0, 0);
-	}
-
-	public boolean check(int[][] matrix) {		
-		for (int x = 0; x < 9; x++) {
-			for (int y = 0; y < 9; y++) {
-				int nbr = matrix[x][y];
-				for (int len = 0; len < 9; len++) {
-					if (matrix[x][len] == nbr && len != y) {
-						return false;
-					}
-					if (matrix[len][y] == nbr && len != x) {
-						return false;
-					}
-				}
-				int row = (x / 3) * 3;
-				int col = (y / 3) * 3;
-				for (int i = 0; i < 3; i++) {
-					for (int k = 0; k < 3; k++) {
-
-						if ((row + i != x && col + k != y) && matrix[row + i][col + k] == nbr) {
-							return false;
-						}
-					}
-				}
-			}
-		}
-		return true;
-
-	}
-
-	public int[][] SolvedMatrix() {
-		solve();
-		return  check(sudokuMatrix) ? sudokuMatrix : null;
-
-	}
-
-	/**
-	 * Checks if the sudoku rules apply.
-	 * 
-	 * @param row    The row of the number.
-	 * @param column The column of the number.
-	 * @param number The number to be checked.
-	 * @return True if the rules apply for the number to be added, False if the
-	 *         rules don't apply.
-	 */
-	private boolean checkRules(int row, int column, int number) {
-
-		return rowAvailabilityCheck(row, number) && columnAvailabilityCheck(column, number)
-				&& boxAvailabilityCheck(row, column, number);
-
-	}
-
-	/**
-	 * Check if the number can be added to the Row.
-	 * 
-	 * @param row    The row of the number to be added.
-	 * @param number The number to be added.
-	 * @return True if the number could be added, False if the number couldn't be
-	 *         added.
-	 */
-	private boolean rowAvailabilityCheck(int row, int number) {
-
-		for (int i = 0; i < 9; i++) {
-			if (sudokuMatrix[row][i] == number)
-				return false;
-		}
-
-		return true;
-
-	}
-
-	/**
-	 * Check if the number can be added to the Column.
-	 * 
-	 * @param column The column of the number to be added.
-	 * @param number The number to be added.
-	 * @return True if the number could be added, False if the number couldn't be
-	 *         added.
-	 */
-	private boolean columnAvailabilityCheck(int column, int number) {
-
-		for (int i = 0; i < 9; i++) {
-			if (sudokuMatrix[i][column] == number)
-				return false;
-		}
-
-		return true;
-	}
-
-	/**
-	 * Check if the number can be added to the 3x3 box.
-	 * 
-	 * @param row    The row of the number to be added.
-	 * @param column The column of the number to be added.
-	 * @param number The number to be added.
-	 * @return True if the number could be added, False if the number couldn't be
-	 *         added.
-	 */
-	private boolean boxAvailabilityCheck(int row, int column, int number) {
-
-		int rr = row - row % 3;
-		int cc = column - column % 3;
-
-		for (int i = rr; i < rr + 3; i++) {
-			for (int j = cc; j < cc + 3; j++) {
-				if (sudokuMatrix[i][j] == number)
-					return false;
-			}
-		}
-		return true;
 	}
 
 	/**
@@ -138,7 +38,9 @@ public class soduku_class {
 	 *         get solved
 	 */
 	private boolean solve(int row, int column) {
-
+			/*
+			 * stop condition for recursion
+			 */
 		if (row == 9)
 			return true;
 
@@ -147,7 +49,7 @@ public class soduku_class {
 				return true;
 		} else {
 			for (int i = 0; i < 9; i++) {
-				if (checkRules(row, column, i + 1)) {
+				if (checkRules(sudokuMatrix, row, column, i + 1)) {
 					sudokuMatrix[row][column] = (i + 1);
 					if (solve(column == 8 ? (row + 1) : row, (column + 1) % 9))
 						return true;
@@ -160,6 +62,77 @@ public class soduku_class {
 
 		return false;
 
+	}
+
+	/**
+	 * @return true if all is correct
+	 */
+	private boolean check() {
+		for (int x = 0; x < 9; x++) {
+			for (int y = 0; y < 9; y++) {
+				int nbr = sudokuMatrix[x][y];
+				for (int len = 0; len < 9; len++) {
+					if (sudokuMatrix[x][len] == nbr && len != y) {
+						return false;
+					}
+					if (sudokuMatrix[len][y] == nbr && len != x) {
+						return false;
+					}
+				}
+				int row = (x / 3) * 3;
+				int col = (y / 3) * 3;
+				for (int i = 0; i < 3; i++) {
+					for (int k = 0; k < 3; k++) {
+
+						if ((row + i != x && col + k != y) && sudokuMatrix[row + i][col + k] == nbr) {
+							return false;
+						}
+					}
+				}
+			}
+		}
+		return true;
+
+	}
+
+	/**
+	 * @return the matrix to be printed
+	 */
+	public int[][] SolvedMatrix() {
+		solve();
+		return check() ? sudokuMatrix : null;
+
+	}
+
+	/**
+	 * Checks if the sudoku rules apply.
+	 * 
+	 * @param row    The row of the number.
+	 * @param column The column of the number.
+	 * @param number The number to be checked.
+	 * @return True if the rules apply for the number to be added, False if the
+	 *         rules don't apply.
+	 */
+	public boolean checkRules(int[][] matrix, int x, int y, int nbr) {
+
+		for (int i = 0; i < 9; i++) {
+			if (matrix[x][i] == nbr && y != i) {
+				return false;
+			}
+			if (matrix[i][y] == nbr && x != i) {
+				return false;
+			}
+		}
+		int row = (x / 3) * 3;
+		int col = (y / 3) * 3;
+		for (int i = 0; i < 3; i++) {
+			for (int k = 0; k < 3; k++) {
+				if ((row + i != x && col + k != y) && matrix[row + i][col + k] == nbr) {
+					return false;
+				}
+			}
+		}
+		return true;
 	}
 
 }
